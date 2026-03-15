7d661e110b368ad35319dbe8b9c49292c43daef3
nikitakrapo <n.krapotkin@gmail.com>
2026-03-15 01:12:42 +0000

    Add network logging

--- features/network/src/commonMain/kotlin/com/nikitakrapo/progressif/network/HttpClientFactory.kt
+++ features/network/src/commonMain/kotlin/com/nikitakrapo/progressif/network/HttpClientFactory.kt
@@ -1,10 +1,14 @@
 package com.nikitakrapo.progressif.network
 
+import io.github.aakira.napier.Napier
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -31,6 +35,14 @@ class HttpClientFactory(
             }
             install(HttpTimeout) {
                 requestTimeoutMillis = networkConfig.timeout.inWholeMilliseconds
+            }
+            install(Logging) {
+                level = LogLevel.INFO
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Napier.v(tag = "HttpClient", message = message)
+                    }
+                }
             }
         }
     }
