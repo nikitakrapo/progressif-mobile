9a6250bf73dd86ba3d67ea47d940dea2f8436c20
nikitakrapo <n.krapotkin@gmail.com>
2026-05-10 18:33:08 +0100

    Changed base url

--- integration-core/src/commonMain/kotlin/com/nikitakrapo/progressif/network/ProgressifNetworkConfig.kt
+++ integration-core/src/commonMain/kotlin/com/nikitakrapo/progressif/network/ProgressifNetworkConfig.kt
@@ -3,6 +3,6 @@ package com.nikitakrapo.progressif.network
 import kotlin.time.Duration.Companion.seconds
 
 internal val ProgressifNetworkConfig = NetworkConfig(
-    baseUrl = "http://10.0.0.2:8080",
+    baseUrl = "https://api.nikitakrapo.com",
     timeout = 15.seconds,
 )
